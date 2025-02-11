bcb80734ae31140f96eb18101c2c280bcd7fff90
LucasCostaGoncalves <lc915601@gmail.com>
2025-02-11 20:17:24 -0300

    Add files via upload

---
+++ calculadora.sh
@@ -0,0 +1,25 @@
+ #!/bin/bash
+
+echo "Digite o primeiro número: "
+read num1
+echo "Digite o segundo número: "
+read num2
+
+echo "Escolha a operação (+, -, *, /): "
+read operacao
+
+case $operacao in
+  +) resultado=$(echo "$num1 + $num2" | bc);;
+  -) resultado=$(echo "$num1 - $num2" | bc);;
+  \*) resultado=$(echo "$num1 * $num2" | bc);;
+  /) resultado=$(echo "$num1 / $num2" | bc);;
+  *) echo "Operação inválida"; exit 1;;
+esac
+
+echo "Resultado": $resultado
+
+chmod +x calculadora.sh
+
+chmod 744 calculadora.sh
+
+./calculadora.sh
